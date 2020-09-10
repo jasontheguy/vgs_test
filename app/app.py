6898710345e392ffb948fa7a4e2c12bd05834dac
Jason Renaud <jjrenaud@protonmail.com>
2020-09-10 12:30:24 -0400

    Adding all files again

--- app/app.py
+++ app/app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask import render_template
 from flask import render_template, request, jsonify
 import requests
-import os
 import json
 
 app = Flask(__name__)
@@ -17,14 +16,13 @@ def card():
     # This renders the fancy credit card form
     return render_template("card.html")
 
-@app.route('/form')
+@app.route('/send', methods=['GET','POST'])
 def get_form_info():
-    # This creates a small JSON document which is mostly the standard of API calls
-    num = request.form['number']
+    card_number = request.form['number']
     expiration = request.form['expiry']
-    cvv = request.form['cvv']
-    document_as_json={"card_num":num,"cvv":cvv,"exp_date":expiration}
-    y = json.dumps(document_as_json)
-    return y
+    cvv_code=request.form['cvv']
+    info_to_secure = {"card_number":card_number,"expiration":expiration,"cvv_code":cvv_code}
+
 if __name__ == '__main__':
     app.run()
+
