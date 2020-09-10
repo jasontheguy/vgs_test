8f286a7db9cc92f0a2a753ff1adea9a4c160b96b
Jason Renaud <jjrenaud@protonmail.com>
2020-09-10 15:53:03 -0400

    Changed a few endpoints

--- app/app.py
+++ app/app.py
@@ -23,12 +23,16 @@ def generate():
     cvv_code=request.form['cvv']
     #This below takes the form data, which works when I test it, and basically makes it a tiny JSON doc
     info_to_secure = {"card_number":card_number,"expiration":expiration,"cvv_code":cvv_code}
-    #Following along with 
-    res = requests.post('http://bdbdbacbd292.ngrok.io/send',json=info_to_secure)
-    res=res.json()
-    return res
+    #Following along with https://github.com/Stepan-VGS/simple_app_test_vgs/blob/master/app/routes.py, I cannot get this to even pass thru VGS to encode, from looking at the logs.
+    #This is where I am having trouble. This is my understanding:
+    #1) I create a JSON document/field with sensitive data. That is defined above.
+    #2) I send said document to the path that I defined using the ngrok public server endpoint in the VGS dashboard telling it what fields to redact
+    #3) I return it to the VGS echo server to show the data revealed
+    #res = requests.post('http://bdbdbacbd292.ngrok.io/send',json=info_to_secure)
+    #res=res.json()
+    #Opening the PR for feedback from devs/support
+    return info_to_secure
 
 if __name__ == '__main__':
-    #https://blog.miguelgrinberg.com/post/running-your-flask-application-over-https
     app.run()
 
