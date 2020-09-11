91613705176dffe4c1ca777bdb6cb8c29edba58e
Jason Renaud <jjrenaud@protonmail.com>
2020-09-11 11:51:10 -0400

    Updated the route to return tokenized data. Not prettified and used Postman to validate.

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from flask import render_template
 from flask import render_template, request, jsonify
 import requests
 import json
+import pprint
 
 app = Flask(__name__)
 
@@ -21,18 +21,21 @@ def generate():
     card_number = request.form['number']
     expiration = request.form['expiry']
     cvv_code=request.form['cvv']
+    
     #This below takes the form data, which works when I test it, and basically makes it a tiny JSON doc
     info_to_secure = {"card_number":card_number,"expiration":expiration,"cvv_code":cvv_code}
-    #Following along with https://github.com/Stepan-VGS/simple_app_test_vgs/blob/master/app/routes.py, I cannot get this to even pass thru VGS to encode, from looking at the logs.
-    #This is where I am having trouble. This is my understanding:
-    #1) I create a JSON document/field with sensitive data. That is defined above.
-    #2) I send said document to the path that I defined using the ngrok public server endpoint in the VGS dashboard telling it what fields to redact
-    #3) I return it to the VGS echo server to show the data revealed
-    #res = requests.post('http://bdbdbacbd292.ngrok.io/send',json=info_to_secure)
-    #res=res.json()
-    #Opening the PR for feedback from devs/support
-    return info_to_secure
+
+    #Generated with Postman
+    url = "https://tntkp8h2mvu.sandbox.verygoodproxy.com/post"
+    payload = json.dumps(info_to_secure)
+    headers = {'Authorization': 'Basic YXBwbGVzZWVkY2FzdDBAZ21haWwuY29tOkZyb2RvITIz',
+    'Content-Type': 'application/json'
+              }
+    response = requests.request("POST", url, headers=headers, data = payload)
+    json_data=response.json()
+    return render_template('redacted.html',response=json_data.get('data'))
 
 if __name__ == '__main__':
     app.run()
 
+#pprint.pprint(json_data.get('data'))
